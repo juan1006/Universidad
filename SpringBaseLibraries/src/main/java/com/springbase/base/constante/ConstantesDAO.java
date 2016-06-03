76396a484da2e7ebca7298b0e0547a1c7d8bb713
juan1006 <juan_q_0611@hotmail.com>
2016-06-03 12:36:08 -0500

    SpringBaseLibraries
    SpringBaseLibraries

---
+++ SpringBaseLibraries/src/main/java/com/springbase/base/constante/ConstantesDAO.java
@@ -0,0 +1,7 @@
+package com.springbase.base.constante;
+
+public class ConstantesDAO {
+	public static final String SCHEMA_NAME="sch_realplaza";
+	
+	public static final String SP_ROL_LISTAR="sp_rol_listar_roles";
+}
---
+++ SpringBaseLibraries/src/main/java/controller/newa.java
@@ -0,0 +1,9 @@
+package controller;
+
+import com.springbase.base.bean.Product;
+
+public class newa {
+
+	Product a = new Product();
+	
+}
